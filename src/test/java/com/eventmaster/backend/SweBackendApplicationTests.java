0bd4c42ec248d24332a4f609eaacaf24892a7e2a
Fabian_Eilber <fabian.eilber01@gmail.com>
2023-04-10 11:48:12 +0200

    Initial commit

---
+++ src/test/java/com/eventmaster/backend/SweBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventmaster.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
