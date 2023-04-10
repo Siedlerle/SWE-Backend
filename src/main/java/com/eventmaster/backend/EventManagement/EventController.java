53b6d9057a61b47efdbee58a5c2243a2457526c1
Fabian_Eilber <fabian.eilber01@gmail.com>
2023-04-10 12:58:09 +0200

    Anlegen der grundlegenden Struktur

--- src/main/java/com/eventmaster/backend/EventManagement/EventController.java
+++ src/main/java/com/eventmaster/backend/EventManagement/EventController.java
@@ -1,6 +1,7 @@
 package com.eventmaster.backend.EventManagement;
 
 
+import com.eventmaster.backend.UserManagement.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -62,11 +63,37 @@ public class EventController {
      * Gets an id of an event and this will be deleted in the database.
      *
      * @param eventId ID of the event which will be deleted in the database.
+     * @return HTTP response with a body including a boolean as status for succes
      */
     @PostMapping("/delete")
     public ResponseEntity<Boolean> deleteEvent(@PathVariable Long eventId){
         boolean eventDeleted = eventService.deleteEvent(eventId);
         return ResponseEntity.ok(eventDeleted);
+    }
+
+    /**
+     * Gets an id of an event to update it.
+     *
+     * @param eventId ID of the event which will be searched in the database to get updated.
+     * @param event Event Object with all the changes
+     * @return HTTP response with a body including a boolean as status for succes
+     */
+    @PostMapping("/get-participants/{eventId}")
+    public ResponseEntity<Boolean> getParticipants(@PathVariable Long eventId, @RequestBody Event event){
+        boolean eventUpdated = eventService.updateEvent(eventId, event);
+        return ResponseEntity.ok(eventUpdated);
+    }
+
+    /**
+     * Gets an id of an event to search the database for participants.
+     *
+     * @param eventId ID of the event which will be searched in the database.
+     * @return HTTP response with a body including a List of Users.
+     */
+    @PostMapping("/get-participants/{eventId}")
+    public ResponseEntity<List<User>> getParticipants(@PathVariable Long eventId){
+        List<User> participants = eventService.getParticipants(eventId);
+        return ResponseEntity.ok(participants);
     }
 
 
--- src/main/java/com/eventmaster/backend/EventManagement/EventService.java
+++ src/main/java/com/eventmaster/backend/EventManagement/EventService.java
@@ -1,5 +1,6 @@
 package com.eventmaster.backend.EventManagement;
 
+import com.eventmaster.backend.UserManagement.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -53,8 +54,9 @@ public class EventService {
     }
 
     /**
-     * An event corresponding to teh eventId is being deleted
+     * An event corresponding to the eventId is being deleted
      * @param eventId ID of the event which will be deleted.
+     * @return Boolen as status for succes
      */
     public boolean deleteEvent(Long eventId){
         try {
@@ -65,5 +67,34 @@ public class EventService {
             return false;
         }
     }
+
+    /**
+     * An event corresponding to the eventId is being updated
+     * @param eventId ID of the event which will be updated.
+     * @param event Event Object with the changes
+     * @return Boolen as status for succes
+     */
+    public boolean updateEvent(Long eventId, Event event){
+        //Todo Implemetieren, dass das entsprechende Event gefunden wird und mit dem übergebenen Event Objekt geupdated wird
+        try {
+            eventRepository.findById(eventId);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * An event corresponding to the eventId is being searched to return its participants
+     * @param eventId ID of the event which will be updated.
+     * @return List of participants
+     */
+    public List<User> getParticipants(Long eventId){
+        //Todo Implemetieren, dass entsprechende User gefunden werden und zurückgegeben werden
+        List<User> participants = null;
+        return participants;
+    }
+
 
 }
