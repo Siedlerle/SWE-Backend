d1b3c0a862512c46920afbe4a394cbc57098abce
FabUnger <fabian.unger@magenta.de>
2023-04-19 15:48:08 +0200

    Preset Verwaltung hinzugefügt

--- src/main/java/com/eventmaster/backend/repositories/PresetRepository.java
+++ src/main/java/com/eventmaster/backend/repositories/PresetRepository.java
@@ -1,7 +1,10 @@
 package com.eventmaster.backend.repositories;
 
+import com.eventmaster.backend.entities.Organisation;
 import com.eventmaster.backend.entities.Preset;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * This interface is used to access the Presets in the database.
@@ -9,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Fabian Unger
  */
 public interface PresetRepository extends JpaRepository<Preset, Long> {
+    List<Preset> findByOrganisation(Organisation organisation);
+    Preset findById(long presetId);
 }
