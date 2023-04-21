23de51f4e2d45714f9fb211180f77f44a67aa978
Fabian_Eilber <fabian.eilber01@gmail.com>
2023-04-21 20:39:10 +0200

    Strings lokal gesammelt

--- src/main/java/com/eventmaster/backend/services/QuestionService.java
+++ src/main/java/com/eventmaster/backend/services/QuestionService.java
@@ -5,7 +5,7 @@ import com.eventmaster.backend.entities.Question;
 import com.eventmaster.backend.repositories.QuestionRepository;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class QuestionService {
 
     /*
