4096cb79cf860d5aae7c2a1ff4a8a66747f1e6b4
Siedlerle <105000385+Siedlerle@users.noreply.github.com>
2023-04-29 12:46:07 +0200

    Passwordabfrage beim sys-Admin in authorisierung abgefragt

---
+++ src/main/java/com/eventmaster/backend/security/authorization/AdminProperties.java
@@ -0,0 +1,18 @@
+package com.eventmaster.backend.security.authorization;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "admin")
+public class AdminProperties {
+    private String adminPassword;
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+}
--- src/main/java/com/eventmaster/backend/security/config/SecurityConfig.java
+++ src/main/java/com/eventmaster/backend/security/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.eventmaster.backend.security.config;
 
-import com.eventmaster.backend.security.authorization.CustomAuthorizationManager;
-import com.eventmaster.backend.security.authorization.OrganizerAuthorizationManager;
+import com.eventmaster.backend.security.authorization.AdminAuthManager;
+import com.eventmaster.backend.security.authorization.AdminProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,13 +21,16 @@ public class SecurityConfig{
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final LogoutHandler logoutHandler;
+    private final AdminProperties adminProperties;
 
     public SecurityConfig(AuthenticationProvider authenticationProvider,
                           JwtAuthenticationFilter jwtAuthenticationFilter,
-                          LogoutHandler logoutHandler) {
+                          LogoutHandler logoutHandler,
+                          AdminProperties adminProperties) {
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.logoutHandler = logoutHandler;
+        this.adminProperties = adminProperties;
     }
 
 
@@ -52,6 +54,7 @@ public class SecurityConfig{
                     .requestMatchers("/admin/orga/**").permitAll()
                     .requestMatchers("/tutor/**").permitAll()
                     .requestMatchers("/logout").permitAll()
+                .requestMatchers("/sys-admin/**").access(new AdminAuthManager(adminProperties.getAdminPassword()))
                     //.requestMatchers("/organizer/**").access(new OrganizerAuthorizationManager())
                     //.anyRequest().access(new CustomAuthorizationManager())
                 .and()
